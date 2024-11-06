26269dc73ff5f0ee6afb2afbdfd60636f88ab60d
Belabied Redouane <48478700+belabiedredouane@users.noreply.github.com>
2024-11-06 20:57:24 +0100

    Update informations

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:direct_call_plus/main.dart';
+import 'package:phone_direct_caller/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
