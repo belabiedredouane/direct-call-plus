0f94576e40b7b816d9527bf1e6a7e9a827cc0a0e
Belabied Redouane <48478700+belabiedredouane@users.noreply.github.com>
2024-11-10 16:53:38 +0100

    Update direct_call_plus.dart

--- lib/direct_call_plus.dart
+++ lib/direct_call_plus.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+
 /// DirectCallPlus class
 class DirectCallPlus {
   static const MethodChannel _channel = MethodChannel('direct_call_plus');
 
-/// Methode to make call to given number
+  /// Methode to make call to given number
   static Future<bool?> makeCall(String number) async {
     return await _channel.invokeMethod(
       'callNumber',
